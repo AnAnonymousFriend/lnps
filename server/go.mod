c102565ce06f0c9f96948eaf51360a31846df5eb
kai_wang <871779972@qq.com>
2020-09-19 11:53:20 +0800

    swag init

--- server/go.mod
+++ server/go.mod
@@ -3,6 +3,9 @@ module LNPS/server
 go 1.13
 
 require (
+	github.com/gin-gonic/gin v1.6.3
+	github.com/micro/go-micro v1.18.0 // indirect
 	github.com/sirupsen/logrus v1.6.0
+	github.com/swaggo/gin-swagger v1.2.0
 	golang.org/x/sys v0.0.0-20200917073148-efd3b9a0ff20 // indirect
 )
--- server/main.go
+++ server/main.go
@@ -7,16 +7,14 @@ import (
 
 
 func main() {
-
 		ginRouter := routers.Routers()
 		httpServer := web.NewService(
 			web.Name("httpprodservice"),
 			web.Handler(ginRouter),
-			//注册进consul服务中的端口,也是这里我们gin的server地址
 			web.Address(":8000"),
 		)
 
-		httpServer.Init() //加了这句就可以使用命令行的形式去设置我们一些启动的配置
+		httpServer.Init()
 		httpServer.Run()
 
 	}
