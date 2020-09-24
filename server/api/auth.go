c1cf634f7bb225e0da6ff01817fdc51037da15fe
kai_wang <871779972@qq.com>
2020-09-24 17:01:31 +0800

    测试post 请求

--- server/api/auth.go
+++ server/api/auth.go
@@ -9,10 +9,12 @@ import "github.com/gin-gonic/gin"
 // @Param userName path int true "userName"
 // @Param passWord path int true "password"
 // @Success 200 {string} string "{"code":200,"data":{},"msg":"ok"}"
-// @Router /api/v1/login/{id} [Post]
+// @Router /api/v1/login [Post]
 func GetAuth(ctx *gin.Context){
 		userName := ctx.PostForm("userName")
+		passWord := ctx.PostForm("passWord")
 		println(userName)
+		println(passWord)
 
 }
 
@@ -22,8 +24,5 @@ func GetAuth(ctx *gin.Context){
 // @Success 200 {string} string "{"code":200,"data":{},"msg":"ok"}"
 // @Router /api/v1/user [Get]
 func GetUser(ctx *gin.Context){
-	msg := ctx.PostForm("msg")
-	println(msg)
-
 
 }
