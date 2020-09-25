8db279af8ad5c3e0fbd556be180637b9b22664ac
kai_wang <871779972@qq.com>
2020-09-25 15:51:23 +0800

    t添加中间件

--- web/node/app.js
+++ web/node/app.js
@@ -1,9 +1,29 @@
 const Koa = require('koa');
 const app = new Koa();
 
-// 响应
-app.use(ctx => {
-  ctx.body = 'Hello Koa';
+
+// x-responese-time
+
+app.use(async(ctx,next)=>{
+    console.log(`${ctx.request.method} ${ctx.request.url}`); // 打印URL
+    await next(); // 调用下一个middleware
+})
+
+app.use(async(ctx,next) =>{
+    const start = new Date().getTime();
+    await next();
+    const ms = new Date().getTime() - start;
+    console.log(`Time: ${ms}ms`)
+})
+
+
+app.use(async (ctx, next) => {
+    await next();
+    // 设置response的Content-Type:
+    ctx.response.type = 'text/html';
+    // 设置response的内容:
+    ctx.response.body = '<h1>Hello, koa2!</h1>';
 });
 
 app.listen(3000);
+console.log('app started at port 3000...');
