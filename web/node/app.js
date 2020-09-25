cbdc377675752cc33d037c992e487d4c545542b8
kai_wang <871779972@qq.com>
2020-09-25 16:34:27 +0800

    添加post 请求

--- web/node/app.js
+++ web/node/app.js
@@ -2,11 +2,15 @@ const Koa = require('koa');
 
 // 注意require('koa-router')返回的是函数:
 const router = require('koa-router')();
+const bodyParser = require('koa-bodyparser');
 
 const app = new Koa();
 
 
 // x-responese-time
+
+app.use(bodyParser());
+app.use(router.routes());
 
 app.use(async(ctx,next)=>{
     console.log(`${ctx.request.method} ${ctx.request.url}`); // 打印URL
@@ -43,7 +47,21 @@ app.use(async(ctx,next)=>{
     var name = ctx.params.name;
     ctx.response.body = `<h1>Hello, ${name}!</h1>`;
 });
-app.use(router.routes());
+
+router.post('/signin', async (ctx, next) => {
+    var
+        name = ctx.request.body.name || '',
+        password = ctx.request.body.password || '';
+    console.log(`signin with name: ${name}, password: ${password}`);
+    if (name === 'koa' && password === '12345') {
+        ctx.response.body = `<h1>Welcome, ${name}!</h1>`;
+    } else {
+        ctx.response.body = `<h1>Login failed!</h1>
+        <p><a href="/">Try again</a></p>`;
+    }
+});
+
+
 
 
 app.listen(3000);
